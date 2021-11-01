f1fbd6f6bfe12e005490fac51996e4129679fdef
Ali <asadri721@gmail.com>
2021-11-02 00:23:49 +0330

    Implement cart redux functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Home
+                  <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
